411ef8b975ad3c33acab8eb25b3a967c8b788324
Nard <nnard2@uisacad5.uisad.uis.edu>
2018-01-22 20:22:38 -0600

    CSC368 assignment stem creation

---
+++ CSC368/assStem.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+#------------------------------
+# Nathan Nard 
+# nnard2
+# CSC 368 - Assignment 1
+# January 28, 2018
+#------------------------------
---
+++ CSC368/week2/assignment1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#------------------------------
+# Nathan Nard 
+# nnard2
+# CSC 368 - Assignment 1
+# January 28, 2018
+#------------------------------
+echo Problem 1
+
