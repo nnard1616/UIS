af50a599b00d4c7c5ba36369a039220c80f790f7
Nathan <nnard1616@gmail.com>
2018-01-31 15:40:09 -0600

    completed csc385 assignment 3

---
+++ CSC385/module3/MyBinarySearch/src/mybinarysearch/MyBinarySearch.java
@@ -0,0 +1,42 @@
+package mybinarysearch;
+
+/**
+ *
+ * @author Nathan Nard
+ */
+public class MyBinarySearch {
+    public int binarySearch(Comparable[] objArray, Comparable searchObj){
+        int low = 0;
+        int high = objArray.length - 1;
+        int mid = 0;
+        
+        // if array is empty
+        if (objArray.length == 0)
+            return 0;
+        
+        // if query is smaller than first item in array
+        if (searchObj.compareTo(objArray[0]) < 0) 
+            return 0;
+        
+        // if query is greater than last item in array
+        if (searchObj.compareTo( objArray[objArray.length - 1] ) > 0)
+            return objArray.length;
+        
+        // query is between the bounds, see if query is in array
+        while (low <= high){
+            mid = (low + high)/2;
+            
+            if (objArray[mid].compareTo(searchObj) < 0)
+                low = mid + 1;
+            else if (objArray[mid].compareTo(searchObj) > 0)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        
+        // query was not present in array, and is between the bounds.
+        //    even length array, add 1 to result
+        //    odd  length array, return result as is
+        return (objArray.length % 2 == 0) ? ++mid : mid;
+    }
+}
---
+++ CSC385/module3/MyBinarySearch/test/mybinarysearchtest/MyBinarySearchTest.java
@@ -0,0 +1,144 @@
+package mybinarysearchtest;
+
+/**
+ *
+ * @author Nathan Nard
+ */
+
+import mybinarysearch.MyBinarySearch;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyBinarySearchTest {
+    private final Integer[] odd     = new Integer[] {3, 5, 6, 8, 9};
+    private final Integer[] even    = new Integer[] {3, 4, 5, 6, 8, 9};
+    private final Integer[] empty   = new Integer[] {};
+    private final MyBinarySearch bs = new MyBinarySearch();
+    
+    @Test
+    public void querySmallerThanLowerBoundShouldReturnZeroForEvenArray(){
+        //Given
+        Integer query = 2;
+        
+        //Then
+        Assert.assertEquals("Query smaller than even numbered array's lower bound should return zero.", 
+                            0, 
+                            bs.binarySearch(even, query));
+    }
+    
+    @Test
+    public void querySmallerThanLowerBoundShouldReturnZeroForOddArray(){
+        //Given
+        Integer query = 2;
+        
+        //Then
+        Assert.assertEquals("Query smaller than odd numbered array's lower bound should return zero.", 
+                            0, 
+                            bs.binarySearch(odd, query));
+    }
+    
+    @Test
+    public void queryEqualToLowerBoundShouldReturnZeroForEvenArray(){
+        Assert.assertEquals("Query equal to the even numbered array's lower bound should return zero.", 
+                            0, 
+                            bs.binarySearch(even, even[0]));
+    }
+    
+    @Test
+    public void queryEqualToLowerBoundShouldReturnZeroForOddArray(){
+        Assert.assertEquals("Query equal to the odd numbered array's lower bound should return zero.", 
+                            0, 
+                            bs.binarySearch(odd, odd[0]));
+    }
+    
+    @Test
+    public void queryLargerThanUpperBoundShouldReturnArrayLengthForEvenArray(){
+        //Given
+        Integer query = 10;
+        
+        //Then
+        Assert.assertEquals("Query greater than even numbered array's upper bound should return array length.", 
+                            even.length, 
+                            bs.binarySearch(even, query));
+    }
+    
+    @Test
+    public void queryLargerThanUpperBoundShouldReturnArrayLengthForOddArray(){
+        //Given
+        Integer query = 10;
+        
+        //Then
+        Assert.assertEquals("Query greater than odd numbered array's upper bound should return array length.", 
+                            odd.length, 
+                            bs.binarySearch(odd, query));
+    }
+    
+    @Test
+    public void queryEqualToUpperBoundShouldReturnLastIndexForEvenArray(){
+        Assert.assertEquals("Query equal to even numbered array's upper bound should return last index.", 
+                            even.length-1, 
+                            bs.binarySearch(even, even[even.length-1]));
+    }
+    
+    @Test
+    public void queryEqualToUpperBoundShouldReturnLastIndexForOddArray(){
+        Assert.assertEquals("Query equal to odd numbered array's upper bound should return last index.", 
+                            odd.length-1, 
+                            bs.binarySearch(odd, odd[odd.length-1]));
+    }
+    
+    @Test
+    public void nonExistentQueryBetweenLowerAndUpperBoundShouldReturnIntermediateForEvenArray(){
+        //Given
+        Integer query = 7;
+        
+        //Then 
+        Assert.assertEquals("Non existent query should return the proper index for orderly insertion into even numbered array.", 
+                            4, 
+                            bs.binarySearch(even, query));
+    }
+    
+    @Test
+    public void nonExistentQueryBetweenLowerAndUpperBoundShouldReturnIntermediateForOddArray(){
+        //Given
+        Integer query = 7;
+        
+        //Then 
+        Assert.assertEquals("Non existent query should return the proper index for orderly insertion into odd numbered array.", 
+                            3, 
+                            bs.binarySearch(odd, query));
+    }
+    
+    @Test
+    public void queryBetweenLowerAndUpperBoundShouldReturnIntermediateForEvenArray(){
+        //Given
+        Integer query = 8;
+        
+        //Then
+        Assert.assertEquals("Query should return the proper index for orderly insertion into even numbered array.", 
+                            4, 
+                            bs.binarySearch(even, query));
+    }
+    
+    @Test
+    public void queryBetweenLowerAndUpperBoundShouldReturnIntermediateForOddArray(){
+        //Given
+        Integer query = 8;
+        
+        //Then
+        Assert.assertEquals("Query should return the proper index for orderly insertion into odd numbered array.", 
+                            3, 
+                            bs.binarySearch(odd, query));
+    }
+    
+    @Test
+    public void queryOfEmptyListShouldReturnZero(){
+        //Given
+        Integer query = 5;
+        
+        //Then
+        Assert.assertEquals("Binary Search should return 0 for an empty list.", 
+                            0, 
+                            bs.binarySearch(empty, query));
+    }
+}
